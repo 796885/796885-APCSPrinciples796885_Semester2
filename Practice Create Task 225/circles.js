e133a95570fd6cc4b0cb37dc01beed3b6c1e3344
796885 <796885@seq.org>
2020-02-28 08:50:43 -0800

    hyht

--- Practice Create Task 225/circles.js
+++ Practice Create Task 225/circles.js
@@ -1,1 +1,38 @@
-class Circles
+class Circles{
+constructor(x, y, dx, dy, id){
+  this.loc = createVector(x, y);
+  this.vel = createVector(dx, dy);
+  this.acc = createVector(0,0);
+  this.clr = color(random(255), random(255), random(255));
+}
+
+run(){
+  this.checkedges();
+  this.update();
+  this.render();
+}
+
+checkedges(){
+  if(this.loc.x < 0){
+    this.vel.x= -this.vel.x
+  }
+  if(this.loc.x> width){
+    this.vel.x= -this.vel.x
+  }
+  if(this.loc.y < 0){
+    this.vel.y= -this.vel.y
+  }
+  if(this.loc.y> height){
+    this.vel.y= -this.vel.y
+  }
+}
+
+update(){
+  this.loc.add(this.vel);
+}
+
+render(){
+  fill(this.clr);
+  ellipse(this.loc.x, this.loc.y,12,12);
+}
+}
--- Practice Create Task 225/sketch.js
+++ Practice Create Task 225/sketch.js
@@ -9,8 +9,9 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-loadObjects(1);
-
+loadBalls(1);
+loadCircles(3);
+loadTriangles(3);
 }
 
 //  The draw function is called @ 30 fps
@@ -22,11 +23,16 @@ function loadObjects(n){
   paddle= new Paddle(50,400,95,95);
   for(var i=0; i< n; i++){
     balls[i] =new Ball(random(width),random(height), random(-5,5),random(-5,5));
+    circles[i]=new Circles(random(width), random(height), random (-1,1), random(-1,1), i);
+    triangles[i]=new Triangles(random(width), random(height), random (-1,1), random(-1,1), i);
   }
 }
 function runObjects(){
   paddle.run();
   for(var i=0; i<balls.length; i++){
     balls[i].run();
+    Circles[i].run();
+    Triangles[i].run();
+
   }
 }
